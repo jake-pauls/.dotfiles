ab3bbc193162f230b559b89abbba5bebacd41c48
jacob-pauls <jakepauls99@gmail.com>
2021-04-20 15:51:27 -0700

    Added yay conf and pkg conf scripts, modified some checklist items

---
+++ confpkg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# These are the packages required for my dotfiles configuration
+# Obviously, this script assumes you've completed a basic Arch installation 
+
+##########################
+## Package Installation ##
+##########################
+
+pacman -S xorg xmonad xmonad-contrib lightdm picom nitrogen xterm 
