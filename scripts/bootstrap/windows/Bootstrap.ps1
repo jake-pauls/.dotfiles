aa84e7e264f7a658bd7c8f15143466f5b736a7ba
jake-pauls <jakepauls99@gmail.com>
2023-11-19 17:53:52 -0700

    Revisiting machine bootstrapping

---
+++ scripts/bootstrap/windows/Bootstrap.ps1
@@ -0,0 +1,30 @@
+## jake pauls
+## https://jakepauls.dev
+## https://github.com/jake-pauls
+
+##########################
+## Package Installation ##
+##########################
+
+# Install Chocolatey
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install Packages in 'ChocoPackages.txt'
+$packages = Get-Content ".\ChocoPackages.txt"
+foreach($package in $packages) {
+    Write-Host "[info] Installing Package: $package"
+    choco install --yes $package
+}
+
+##############
+## Symlinks ##
+##############
+
+# Config Directory
+md -Force $HOME\.config
+
+# Starship
+ni $HOME\.dotfiles\config\starship\starship.toml -i SymbolicLink -ta "$HOME\.config\"
+
+# Default PowerShell Configuration
+ni $HOME\.dotfiles\misc\windows\Microsoft.PowerShell_profile.ps1 -i SymbolicLink -ta "$HOME\Documents\WindowsPowerShell"
