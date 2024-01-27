b49e5531a3062d667f67fec78d389735f5b6efb3
jake-pauls <jakepauls99@gmail.com>
2024-01-26 21:37:53 -0500

    added terminal bindings to remaps

--- config/nvim/lua/base/remap.lua
+++ config/nvim/lua/base/remap.lua
@@ -5,5 +5,10 @@ keymap.set({ "n", "x" }, "<leader>y", '"+y', { desc = "base: Yank to System Clip
 -- Paste from system clipboard
 keymap.set({ "n", "x" }, "<leader>p", '"+p', { desc = "base: Paste from System Clipboard", })
 
+-- Release Focus on Terminal Buffers
+keymap.set({ "t" }, "<Esc>", "<C-\\><C-n>", { desc = "base: Release Focus on Terminal Buffer", })
+-- Open Terminal Buffer in a Vertical Split
+keymap.set({ "n" }, "<C-t>", ":vsplit | term<CR>", { noremap = true }, { desc = "base: Open Terminal in Split" })
+
 -- Update Lazy
 keymap.set({ "n" }, "<leader>ul", "<cmd>Lazy update<CR>", { desc = "base: Update Lazy", })
